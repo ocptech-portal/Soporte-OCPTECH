1b456128700fef948b5f236db4f99b9ce70f222f
Vladimiro Lerena <vllerena@cisco.com>
2026-05-17 22:38:55 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
   Para produccion, no expongas el token de la Service App en el navegador.
   La generacion de guest token y call token deberia hacerse desde un backend.
 */
-let service_app_token = 'Njc4MjNmNWUtOTJlYy00ZGYxLTlhNTctMjQ4NWM3NGI0OGI0ZGNiNjI2M2ItMDcy_P0A1_cbbe27d4-1f60-43cb-819b-fd3749c66621';
+let service_app_token = 'MDViY2M5OTgtNGY5Mi00NmQxLTgwMTQtN2NkNmE2ZTU5MWJjYjhjMzFhMTItZWRh_P0A1_cbbe27d4-1f60-43cb-819b-fd3749c66621';
 const CLICK_TO_CALL_CALLED_NUMBER = '6100';
 const CLICK_TO_CALL_GUEST_NAME = 'Unidos por la colaboración';
 const WEBEX_DISCOVERY_REGION = 'US-EAST';
